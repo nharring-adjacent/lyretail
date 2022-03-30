64050217fb51c7ec0c263b79e38ac48f763be760
Nick Harring <furiouseyebrows@gmail.com>
2022-03-29 17:01:12 -0700

    refactor into multi-threaded tui app

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,35 @@
+extern crate tracing;
+mod app;
 mod args;
+mod ui;
 
-use std::{
-    io::{stdout, Write},
-    pin::Pin,
-    sync::{Arc, RwLock},
-};
+use std::sync::Arc;
 
+use app::LyreTail;
 use clap::Parser;
-use crossterm::{
-    queue,
-    style::Print,
-    terminal::{Clear, ClearType},
-};
 use drain_flow::SimpleDrain;
-use joinery::JoinableIterator;
-use tokio::{
-    fs::File,
-    io::{stdin, AsyncBufRead, AsyncBufReadExt, BufReader},
-    task, time,
-};
+use parking_lot::{RwLock, Mutex};
+use tracing::debug;
+
+use ui::Ui;
 
 use crate::args::Args;
 
 #[tokio::main]
 async fn main() {
-    let args = Args::parse();
+    // console_subscriber::init();
+    tracing_subscriber::fmt::init();
+
+    let args = Arc::new(Mutex::new(Args::parse()));
+    debug!("got args");
     let drain = Arc::new(RwLock::new(SimpleDrain::new(vec![]).unwrap()));
-    let exit_drain = drain.clone();
-    ctrlc::set_handler(move || println!("{}", print_groups(&exit_drain.read().unwrap()).unwrap()))
-        .expect("Installing ctrl-c handler works");
-    let mut buffer = String::new();
-    if args.periodic {
-        let period_drain = drain.clone();
-        let period_interval = args.interval.clone();
-        task::spawn(async move {
-            print_update(period_interval, period_drain).await;
-        });
-    };
-    let mut reader = Box::pin(BufReader::new(stdin())) as Pin<Box<dyn AsyncBufRead>>;
-    if let Some(file_path) = args.file {
-        reader = Box::pin(BufReader::new(File::open(file_path).await.unwrap()));
-    }
-    while let Ok(b) = reader.read_line(&mut buffer).await {
-        if b == 0 {
-            break;
-        }
-        let _ = drain.write().unwrap().process_line(buffer.clone()).unwrap();
-        buffer.clear();
-    }
-    if args.periodic {
-        let mut stdout = stdout();
-        queue!(stdout, Clear(ClearType::All)).unwrap();
-        stdout.flush().unwrap();
-    }
-    if let Some(s) = print_groups(&drain.read().unwrap()) {
-        println!("{}", s);
-    };
-}
+    debug!("got drain");
+    let app = LyreTail::create_app(Some(drain), args).unwrap();
+    debug!("got app");
 
-fn print_groups(drain: &SimpleDrain) -> Option<String> {
-    let groups = drain.iter_groups();
-    Some(format!(
-        "{}",
-        groups
-            .iter()
-            .flatten()
-            .map(|g| { g.to_string() })
-            .join_with("\n")
-            .to_string()
-    ))
-}
-
-async fn print_update(freq: chrono::Duration, drain: Arc<RwLock<SimpleDrain>>) {
-    let mut interval = time::interval(freq.to_std().expect("chrono can convert to std duration"));
-    let mut stdout = stdout();
-    loop {
-        interval.tick().await;
-        if let Some(s) = print_groups(&drain.read().unwrap()) {
-            queue!(stdout, Clear(ClearType::All), Print(s)).unwrap();
-            stdout.flush().unwrap();
-        }
-    }
+    app.run();
+    debug!("app running");
+    let ui = Ui::new(&app);
+    debug!("got ui");
+    ui.run_ui().unwrap();
 }
---
+++ src/ui.rs
@@ -0,0 +1,121 @@
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::thread::sleep;
+use std::{io::stdout, sync::Arc};
+
+use anyhow::Error;
+use chrono::Duration;
+use itertools::Itertools;
+use parking_lot::RwLock;
+use tracing::{debug, instrument};
+use tui::{
+    backend::{Backend, CrosstermBackend},
+    layout::{Constraint, Layout},
+    style::{Color, Modifier, Style},
+    widgets::{Block, Borders, Cell, Row, Table, TableState},
+    Frame, Terminal,
+};
+
+use crossterm::{
+    event::{DisableMouseCapture, EnableMouseCapture},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+
+use crate::app::LyreTail;
+
+#[derive(Clone, Debug)]
+pub(crate) struct Ui<'a> {
+    state: Arc<RwLock<TableState>>,
+    stopping: Arc<AtomicBool>,
+    app: &'a LyreTail,
+}
+
+impl<'a> Ui<'a> {
+    #[instrument]
+    pub fn new(app: &'a LyreTail) -> Self {
+        Self {
+            state: Arc::new(RwLock::new(TableState::default())),
+            stopping: Arc::new(AtomicBool::new(false)),
+            app,
+        }
+    }
+
+    #[instrument]
+    pub(crate) fn run_ui(&self) -> Result<(), Error> {
+        let h = self.stopping.clone();
+        ctrlc::set_handler(move || {
+            debug!("caught ctrl-c, setting stopping flag");
+            h.store(true, Ordering::SeqCst);
+        })
+        .expect("Installing ctrl-c handler works");
+        // setup terminal
+        // enable_raw_mode()?;
+        let mut stdout = stdout();
+
+        execute!(stdout, EnterAlternateScreen)?;
+        let backend = CrosstermBackend::new(stdout);
+        let mut terminal = Terminal::new(backend)?;
+        loop {
+            if self.stopping.load(Ordering::SeqCst) {
+                debug!("stopping flag seen, exiting loop");
+                break;
+            }
+            terminal.draw(|f| self.render_ui(f))?;
+            sleep(Duration::milliseconds(50).to_std()?);
+        }
+        {
+            let _rs = debug!("restoring terminal");
+            // disable_raw_mode()?;
+            execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
+            terminal.show_cursor()?;
+        }
+        debug!("run_ui complete");
+        Ok(())
+    }
+
+    fn render_ui<B: Backend>(&self, f: &mut Frame<B>) {
+        debug!("starting render_ui");
+        let rects = Layout::default()
+            .constraints([Constraint::Percentage(100)].as_ref())
+            .margin(5)
+            .split(f.size());
+        let header_cells = ["ID", "Event", "Quantity Seen"]
+            .iter()
+            .map(|h| Cell::from(*h).style(Style::default().fg(Color::Red)));
+        let selected_style = Style::default().add_modifier(Modifier::REVERSED);
+        let normal_style = Style::default().bg(Color::Blue);
+        let header = Row::new(header_cells)
+            .style(normal_style)
+            .height(1)
+            .bottom_margin(1);
+        let rows = self
+            .app
+            .get_drain_ref()
+            .read()
+            .iter_groups()
+            .iter()
+            .flatten()
+            .sorted_by(|a, b| Ord::cmp(&b.len(), &a.len()))
+            .map(|lg| {
+                let cells = vec![
+                    Cell::from(lg.event().uid.serialize()),
+                    Cell::from(lg.event().to_string()),
+                    Cell::from(lg.len().to_string()),
+                ];
+                Row::new(cells).height(1).bottom_margin(1)
+            })
+            .collect::<Vec<Row>>();
+        let t = Table::new(rows)
+            .header(header)
+            .block(Block::default().borders(Borders::ALL).title("LogGroups"))
+            .highlight_style(selected_style)
+            .highlight_symbol(">> ")
+            .widths(&[
+                Constraint::Percentage(20),
+                Constraint::Percentage(70),
+                Constraint::Percentage(10),
+            ]);
+        debug!("finished building table");
+        f.render_stateful_widget(t, rects[0], &mut self.state.clone().write());
+    }
+}
