a2ce37bd889042a9159517ba3344e5e284a698a3
Nick Harring <furiouseyebrows@gmail.com>
2022-03-29 17:32:08 -0700

    embed demo in readme

--- demo.sh
+++ demo.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-
-RUST_LOG=error cargo run -- --interval 500ms --file <(while cat /var/log/wifi.log; do :; done)
+set -eo pipefail
+if [ -z "$1" ]
+ then
+    FILE=/var/log/wifi.log
+else
+    FILE=$1
+fi
+RUST_LOG=error cargo run -- --interval 500ms --file <(while cat $FILE; do :; done)
