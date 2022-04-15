7b657da38d41ee82323bf5a37c221aeec7d18d6a
Nick Harring <furiouseyebrows@gmail.com>
2022-04-14 17:30:07 -0700

    Fix license as SSPL

--- src/ui/log_group.rs
+++ src/ui/log_group.rs
@@ -1,3 +1,13 @@
+// Copyright Nicholas Harring. All rights reserved.
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the Server Side Public License, version 1, as published by MongoDB, Inc.
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the Server Side Public License for more details. You should have received a copy of the
+// Server Side Public License along with this program.
+// If not, see <http://www.mongodb.com/licensing/server-side-public-license>.
+
 use std::sync::Arc;
 
 use crossterm::event::{Event, KeyCode, KeyModifiers};
@@ -30,7 +40,7 @@ impl LogGroupTab {
             .split(f.size());
         let lines = vec![Spans::from(vec![Span::raw(format!(
             "Log Group: {}",
-            self.lg
+            self.lg.get_id()
         ))])];
         let para = Paragraph::new(lines)
             .block(Block::default().title("Log Group"))
